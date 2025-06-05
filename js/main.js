f557cb32eb97e8f31761531ff11504ff921fcf7d
LucaCiani <lucaciani88@gmail.com>
2025-06-06 00:56:21 +0200

    complete + bonus

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 const button = document.getElementById("bottone");
 
-const lampOff = document.querySelector(".lamp-off");
+const image = document.createElement("img");
 
-const lampOn = document.querySelector(".lamp-on");
+const figure = document.getElementById("lamps");
+
+const switchButton = document.getElementById("switch-button");
+
+figure.append(image);
+image.src = "./img/white_lamp.png";
+image.className = "d-block";
+
+button.addEventListener("click", () => {
+    if (image.src.includes("yellow_lamp.png")) {
+        image.src = "./img/white_lamp.png";
+        switchButton.innerHTML = "ACCENDI";
+    } else {
+        image.src = "./img/yellow_lamp.png";
+        switchButton.innerHTML = "SPEGNI";
+    }
+
+    button.classList.toggle("btn-danger");
+});
